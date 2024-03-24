a9dd05a5e6eb4e08d67ac417dbe6a7070bc8623a
Sujan Adhikari <sujan.ad6473@gmail.com>
2024-03-24 22:24:39 +0300

    add style on note page

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     # path('home', views.home),
+    # path('authorized', views.authorized),
+    
     path('home', views.HomeView.as_view()),
     path('authorized', views.AuthoriizdView.as_view),
-    # path('authorized', views.authorized),
+
 ]
--- notes/urls.py
+++ notes/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # path('notes', views.list),
     # path('notes/<int:pk>', views.detail),
 
-    path('notes', views.NoteListView.as_view()),
-    path('notes/<int:pk>', views.NoteDetailsView.as_view()),
+    path('notes', views.NoteListView.as_view(), name="notes.list"),
+    path('notes/<int:pk>', views.NoteDetailsView.as_view(), name="notes.detail"),
 ]
