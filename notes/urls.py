d18daec018a836037e20a8146cfe3cd08e6ea112
Sujan Adhikari <sujan.ad6473@gmail.com>
2024-03-29 23:20:14 +0300

    adding notes form to add new note

--- notes/urls.py
+++ notes/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('notes', views.NoteListView.as_view(), name="notes.list"),
     path('notes/<int:pk>', views.NoteDetailsView.as_view(), name="notes.detail"),
-    path('note/new',views.NoteCreateView.as_view(), name = "notes.new"),
+    path('notes/new',views.NoteCreateView.as_view(), name = "notes.new"), #CRUD
     
     # path('notes', views.list),
     # path('notes/<int:pk>', views.detail),
