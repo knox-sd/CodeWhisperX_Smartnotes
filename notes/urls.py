62e3e6dc119c624ead03060b9667ca6aa1779c54
Sujan Adhikari <sujan.ad6473@gmail.com>
2024-03-30 23:11:13 +0300

    update edit, cancel, delete action on notes

--- notes/urls.py
+++ notes/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('notes', views.NoteListView.as_view(), name="notes.list"),
     path('notes/<int:pk>', views.NoteDetailsView.as_view(), name="notes.detail"),
+    path('notes/<int:pk>/edit', views.NoteUpdateView.as_view(), name="notes.update"),
+    path('notes/<int:pk>/delete', views.NoteDeleteView.as_view(), name="notes.delete"),
     path('notes/new',views.NoteCreateView.as_view(), name = "notes.new"), #CRUD
     
     # path('notes', views.list),
--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import Http404 #if the page not found or page doesn't exist
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
 
 from .forms import NotesForm
 from .models import Notes
 
 # Class base view
+class NoteDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html' #after adding templete only delete will work
+
+class NoteUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm #add forms
+
+
 class NoteCreateView(CreateView): #Apply CRUD
     model = Notes
     # fields = ['title', 'text']
