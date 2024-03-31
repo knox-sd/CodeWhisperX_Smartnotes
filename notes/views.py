02aed4f101cd23c7dbcf5816b86705492a903214
Sujan Adhikari <sujan.ad6473@gmail.com>
2024-03-31 23:15:00 +0300

    add user authorization on notes

--- notes/views.py
+++ notes/views.py
@@ -1,35 +1,48 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
 from django.http import Http404 #if the page not found or page doesn't exist
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
 
 # Class base view
-class NoteDeleteView(DeleteView):
+class NoteDeleteView(LoginRequiredMixin, DeleteView):
     model = Notes
     success_url = '/smart/notes'
     template_name = 'notes/notes_delete.html' #after adding templete only delete will work
 
-class NoteUpdateView(UpdateView):
+class NoteUpdateView(LoginRequiredMixin, UpdateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm #add forms
 
 
-class NoteCreateView(CreateView): #Apply CRUD
+class NoteCreateView(LoginRequiredMixin, CreateView): #Apply CRUD
     model = Notes
     # fields = ['title', 'text']
     success_url = '/smart/notes'
     form_class = NotesForm #add forms
+    
+    def form_valid(self, form):
+        self.object = form.save(commit = False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
-class NoteListView(ListView):
+class NoteListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = "/admin"
 
-class NoteDetailsView(DetailView):
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
+
+class NoteDetailsView(LoginRequiredMixin, DetailView):
     model = Notes
     context_object_name = "note"
 
